5111744e2a25b2c33aae81815e4bc9c9d3d73278
Anastasiya Mashkevich <Anastasiya.Mashkevich@viacomcontractor.com>
2017-11-23 15:40:30 +0300

    5th commit to develop
    # Conflicts:
    #	src/main/java/Develop.java

---
+++ src/main/java/Develop.java
@@ -0,0 +1,9 @@
+
+public class Develop {
+	public void createDevelopBranch() {
+		System.out.println("Develop branch was created. 1st commit to develop.");
+		System.out.println("3d commit to develop.");
+		System.out.println("4th commit to develop.");
+		System.out.println("5th commit to develop.");
+	}
+}
