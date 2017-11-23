7025752967c674ba6e3a1879577d70432d8d7e09
Anastasiya Mashkevich <Anastasiya.Mashkevich@viacomcontractor.com>
2017-11-23 15:15:24 +0300

    3d commit to master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1st commit to master");
-		System.out.println("Second commit to master.");
+		System.out.println("Second commit to master");
+		System.out.println("3d commit to master");
 	}
 }
