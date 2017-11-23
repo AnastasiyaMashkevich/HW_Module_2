95c9d500749791947189a4aa7892f55e6350a6d9
Anastasiya Mashkevich <Anastasiya.Mashkevich@viacomcontractor.com>
2017-11-23 15:17:40 +0300

    Second commit to develop.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 
 public class Main {
 	public static void main(String[] args) {
+		Develop develop = new Develop();
 		System.out.println("1st commit to master");
 		System.out.println("Second commit to master");
 		System.out.println("3d commit to master");
+		develop.createDevelopBranch();
 		System.out.println("4th commit to master");
 	}
 }
