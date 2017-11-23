fffb58062cb72283ec280dd6b1384cb742d968a7
Anastasiya Mashkevich <Anastasiya.Mashkevich@viacomcontractor.com>
2017-11-23 15:20:49 +0300

    4th commit to develop.

--- src/main/java/Develop.java
+++ src/main/java/Develop.java
@@ -3,5 +3,6 @@ public class Develop {
 	public void createDevelopBranch() {
 		System.out.println("Develop branch was created. 1st commit to develop.");
 		System.out.println("3d commit to develop.");
+		System.out.println("4th commit to develop.");
 	}
 }
